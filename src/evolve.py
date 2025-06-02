fd9bd1ef6a30d7be823ca3df400352144e2bf340
Krisi Hristova <kh3339@drexel.edu>
2025-06-02 13:26:54 -0400

    Complete addLimb implementation

--- src/evolve.py
+++ src/evolve.py
@@ -7,16 +7,17 @@ from utils.morphology import *
 import copy
 import math
 import uuid
+import random
 
 """ 
 Mutates a given robot by creating a copy of the robot and augmenting features with 
 randomness of a specific intensity and/or towards a certain target robot. 
 Mutations include changing the number of links and joints, their positions 
 
-:param robot: The robot to mutate
-:param intensity: The amount of randomness to put into mutation
-:param target: The target robot that mutations of the robot to mutate should be moved towards
-:returns: the mutated robot
+@param robot: The robot to mutate
+@param intensity: The amount of randomness to put into mutation
+@param target: The target robot that mutations of the robot to mutate should be moved towards
+@return the mutated robot
 """
 def mutateRobot(robot: Robot, randomness: float = 0.2, target: Robot = None) -> Robot:
 
@@ -37,85 +38,121 @@ def mutateRobot(robot: Robot, randomness: float = 0.2, target: Robot = None) -> 
 
     return mutatedRobot
 
-# Add either a leg to a torso or a torso to a torso. 
+# Helper for rounding in addLimb()
+def rounded(pos, decimals=3):
+    return tuple(round(x, decimals) for x in pos)
+
+"""
+Add a leg to one of 4 corners of a torso. 
+If all corners and center are occupied, returns the original robot. 
+@param robot The robot to add a limb to.
+@returns a mutated robot that has a new limb
+"""
 def addLimb(robot: Robot, randomness: float = 0.1) -> Robot:    
     # Create a copy of the robot that will be mutated. 
     mutatedRobot = copy.deepcopy(robot)
-
     # Find torso link. 
-    links = mutatedRobot.getLinks()
     torsoLink = mutatedRobot.getTorso()
     if torsoLink is None:
-        raise ValueError("Robot has no torso link")
+        raise ValueError("Robot has no torso link.")
     
+    # Find locations that are already occupied.
+    occupiedPositions = set()
+    for joint in mutatedRobot.getJoints():
+        if joint.getParent() == torsoLink:
+            origin = joint.getOrigin()
+            if origin:
+                xyz = rounded(origin.getXyz())
+                occupiedPositions.add(xyz)
+    
+    # Find a location/origin on the torso so the new link won't collide with another.     
+    torsoGeometry = torsoLink.getVisuals()[0].getGeometry()
+    torsoLength = torsoGeometry.getAttributes()['length']
+    torsoRadius = torsoGeometry.getAttributes()['radius']
+    halfLength = torsoLength / 2
+    halfWidth = torsoRadius
+    z = -torsoRadius + 0.005
+
+    offset = torsoRadius * 0.3
+    print(offset)
+    cornerCandidates = [
+        (offset*2, offset, z), #front right (looking from the horizontal side)
+        (-offset*2, offset, z), # front left
+        (offset*2, -offset, z), # back right
+        (-offset*2, -offset, z) # back left
+    ]
+
+    xyz = None
+    for position in cornerCandidates:
+        if rounded(position) not in occupiedPositions:
+            xyz = position
+            print("Placing limb at: ", xyz)
+            break
+    if xyz is None:
+        print("No free torso corners.")
+        if rounded(position) not in occupiedPositions:
+            xyz = (0, 0, z)
+            print("Placing limb at torso center bottom")
+        else:
+            print("No modifications were made. Returning copy of original robot.")
+            return mutatedRobot
+        
     limbId = uuid.uuid4()
     limbName = "link_" + str(limbId) + "_limb"
 
-    # TODO Find a location/origin on the torso so the new link won't collide with another. 
-    # (is not at the same location as another existing link that is connected to the torso).
-    # Make sure it doesn't collide with another limb
     # xyz will be used to set the xyz of Origin objects
-    xyz = (0.03, 0.0, 0.0)
+    limbLength = torsoRadius * 2
+    limbRadius = torsoRadius / 3
 
-    # Visual needs the following: Origin , Geometr, Material - doesn't matter
+    # Visual needs the following: Origin , Geometry, Material - doesn't matter
     limbVisuals = Visual(
-        origin = Origin(xyz, rpy = [0, math.pi/2, 0]),
-        geometry = Capsule(radius = 0.02, length = 0.06),
+        origin = Origin(xyz=xyz, rpy = (0, 0, 0)), #aligned down
+        geometry = Cylinder(radius = limbRadius, length = limbLength),
         material = Material("red", [1.0, 0, 0, 0.9])
     )
     # Collision should be the same as Visual (without Material)
     limbCollisions = Collision(
-        origin = Origin(xyz, rpy = [0, math.pi/2, 0]),
-        geometry = Capsule(radius = 0.02, length = 0.06),
-    )
-    limbInertial = Inertial(
-        mass = 0.1,
-        inertia = (2.0e-4, 0, 0, 2.0e-4, 0, 1.0e-4),
-        origin = Origin(xyz, (0, 0, 0))
+        origin = Origin(xyz=xyz, rpy = (0, 0, 0)), #aligned down
+        geometry = Cylinder(radius = limbRadius, length = limbLength)
     )
     # Create a new limb link.
     limbLink = Link(
         name = limbName, 
-        inertial = limbInertial, 
         visuals = [limbVisuals], 
         collisions = [limbCollisions])
 
     # Create intermediate link. Needs only inertial element. 
     intermediateId = uuid.uuid4()
-    intermediateLinkName = "intermediate_link_" + str(intermediateId) + "_limb" 
-    intermediateInertial = Inertial(
-        mass = 0.001,
-        inertia = (1.0e-6, 0, 0, 1.0e-6, 0, 1.0e-6),
-        origin = Origin((0, 0, 0), (0, 0, 0))
-    )
+    intermediateLinkName = "link_intermediate_" + str(intermediateId) + "_limb" 
 
     intermediateLink = Link(
-        name = intermediateLinkName, 
-        inertial = intermediateInertial)
+        name = intermediateLinkName,
+    )
     
     # Create a new joint (torso to intermediate)
-    jointTorsoToIntermediateName = "joint_" + torsoLink.getName() + "_to_" + intermediateLink.getName()
     jointTorsoToIntermediate = Joint(
-        name = jointTorsoToIntermediateName,
+        name =  "joint_" + torsoLink.getName() + "_to_" + intermediateLink.getName(),
         type = Type.REVOLUTE,
-        parent = torsoLink,          # parent
-        child = intermediateLink,   # child
+        parent = torsoLink,
+        child = intermediateLink,
         origin = Origin(xyz, (0, 0, 0)),
         axis = (0, 0, 1), # Rotation around z-axis for pitch
         limit = Limit(1.0, 2.0, -1.5, 1.5)
     )
+    
     # Create a new joint (intermediate to limb)
-    jointIntermediateToLimbName = "joint_" + intermediateLink.getName() + "_to_" + limbLink.getName()
     jointIntermediateToLimb = Joint(
-        name = jointIntermediateToLimbName,
+        name =  "joint_" + intermediateLink.getName() + "_to_" + limbLink.getName(),
         type = Type.REVOLUTE,
-        parent = intermediateLink,   # parent
-        child = limbLink,           # child
+        parent = intermediateLink,
+        child = limbLink,
         origin = Origin((0, 0, 0), (0, 0, 0)),
         axis = (0, 1, 0), # Rotation around y-axis for yaw
         limit = Limit(1.0, 2.0, -1.5, 1.5)
     )
-    
+    print(jointTorsoToIntermediate.getParent().getName() )
+    print(jointIntermediateToLimb.getParent().getName() )
+
     # Add to the robot torso as a child.
     mutatedRobot.addLink(limbLink)
     mutatedRobot.addLink(intermediateLink)
@@ -139,27 +176,26 @@ def removeLimb(robot: Robot, randomness: float = 0.1) -> Robot:
     # (Note): Removing a torso from a torso for something like a worm. 
     return mutatedRobot
 
-### TODO - Evolve a set of Robots based on a given set and some data
-# Input: List of Tuple of robot object and float. where the float is the score from training. 
-# Output: a list of evolved robot objects. 
-# Create max number of robots we can create.
+"""
+Evolve a set of robots based on their scores from training. 
+@param robots List of Tuples with robot object and float where the float is the score from training. 
+@return a list of evolved robots. 
+"""
 def evolve(robots: list[tuple[Robot, float]]) -> list[Robot]:
     # Duplicate the best one three times, each that changes a random variable.
     # second best we make 2 duplicates of and we do random and change towards the best one.
     # worst performed: least amount of randomness, changes trying to imitate the best performing one. 
 
     # Sort robots into best, worst performing. 
-    print("Robots before sorting.")
-    print(robots)   # DEBUGGING
     robots.sort(key=lambda x: x[1])
-    print("Robots after sorting.")
-    print(robots)   # DEBUGGING
 
     n = len(robots)
     # Best: 20% of the robots,  Worst: 80% of the robots
     if n < 1:
         print("No robots provided")
         return 
+    # Max robots that we are allowed to create. 
+    maxRobots = n**2
     
     bestLength = max(1, int(n * 0.2))
     print("best length: " + str(bestLength))
@@ -173,17 +209,18 @@ def evolve(robots: list[tuple[Robot, float]]) -> list[Robot]:
     for robot, _ in bestRobots:
         evolvedRobots.append(addLimb(robot, randomness=0.2))
         #evolvedRobots.append(removeLimb(robot, randomness=0.2))
-        #evolvedRobots.append(mutateRobot(robot, randomness=0.2))           # Change position of limb
-
+        #evolvedRobots.append(mutateRobot(robot, randomness=0.2))   # Change position of limb
     for robot, _ in worstRobots:
         evolvedRobots.append(mutateRobot(robot, randomness=0.05, target=bestRobots[0][0]))
 
     return evolvedRobots
 
 if __name__ == "__main__":
-    print("Testing main")
     # Generate Robot from test.urdf
-    initialRobot = convertUrdfToRobot("test.urdf")
+    initialRobot = convertUrdfToRobot("simple.urdf")
+    #initialRobot = convertUrdfToRobot("simple_cyl.urdf")
+    initialRobot = convertUrdfToRobot("stompy-5.urdf")
+
     print("Running evolve")
     mutatedRobots = evolve([(initialRobot, 1)])
     print("Converting evolved robots into to URDF")
